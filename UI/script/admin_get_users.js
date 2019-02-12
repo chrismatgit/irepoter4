8c347815ab6118d9b7b9f9f9c87a8f48ceb0f5c5
root <root@localhost.localdomain>
2019-02-12 21:12:38 +0300

    Get all users feature
    In this process, I have been working on how an admin can get all the users.
    - Since the api is already created, now in JS, I added a function that will allow an to get all the users if any created before and if all the validations are passed.
    - The UI interface is responsive.
    
    [Delivers#163901484]

---
+++ UI/script/admin_get_users.js
@@ -0,0 +1,87 @@
+document.getElementById('getall').addEventListener('click', get_all_users)
+
+var token = localStorage.getItem('token')
+const url = 'http://127.0.0.1:5000/api/v1/users'
+let tableBody = document.querySelector('table > tbody')
+
+function get_all_users(event) {
+    event.preventDefault()
+    let invalid = document.getElementById('invalid')
+
+    fetch(url, {
+        method : 'GET',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer ' + token},
+    })
+
+    .then((response) => response.json())
+        .then ((data) => {
+            if (data.status == 200){
+                for (let user of data.Data){
+                    newRow = document.createElement('tr')
+                    newRow.setAttribute("class", "cart-article")
+                    
+                    user_user_id = document.createElement('td')
+                    user_user_id.setAttribute("class", "centered");
+                    user_user_id.textContent = user.user_id
+
+                    user_firstname = document.createElement('td')
+                    user_firstname.setAttribute("class", "centered");
+                    user_firstname.innerHTML = '' + user.firstname
+
+                    user_lastname = document.createElement('td')
+                    user_lastname.setAttribute("class", "centered");
+                    user_lastname.innerHTML = '' + user.lastname
+
+                    user_othernames = document.createElement('td')
+                    user_othernames.setAttribute("class", "centered");
+                    user_othernames.innerHTML = '' + user.othernames
+
+                    user_email = document.createElement('td')
+                    user_email.setAttribute("class", "centered");
+                    user_email.innerHTML = '' + user.email
+
+                    user_phone_number = document.createElement('td')
+                    user_phone_number.setAttribute("class", "centered");
+                    user_phone_number.innerHTML = '' + user.phone_number
+
+                    user_username = document.createElement('td')
+                    user_username.setAttribute("class", "centered");
+                    user_username.innerHTML = '' + user.username
+
+                    user_password = document.createElement('td')
+                    user_password.setAttribute("class", "centered");
+                    user_password.innerHTML = '' + user.password
+
+                    user_registered = document.createElement('td')
+                    user_registered.setAttribute("class", "centered");
+                    user_registered.innerHTML = '' + user.registered
+
+                    user_isadmin = document.createElement('td')
+                    user_isadmin.setAttribute("class", "centered");
+                    user_isadmin.innerHTML = '' + user.isadmin
+
+                    newRow.appendChild(user_user_id)
+                    newRow.appendChild(user_firstname)
+                    newRow.appendChild(user_lastname)
+                    newRow.appendChild(user_othernames)
+                    newRow.appendChild(user_email)
+                    newRow.appendChild(user_phone_number)
+                    newRow.appendChild(user_username)
+                    newRow.appendChild(user_password)
+                    newRow.appendChild(user_registered)
+                    newRow.appendChild(user_isadmin)
+                    tableBody.appendChild(newRow)
+
+                }
+                
+                alert(invalid.textContent = ''+data.message)
+                // window.location.replace('home.html')
+            }else{
+                invalid.textContent = '' + data.error
+            }
+            console.log(data)
+        })
+    .catch(error => console.log(error)), invalid.textContent = "error"
+
+}
